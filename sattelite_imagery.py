0809572910caed6bf6fb98195e229c869ed5d749
Andrey <78215195+821125@users.noreply.github.com>
2022-08-17 12:24:35 +0300

    optimize

--- sattelite_imagery.py
+++ sattelite_imagery.py
@@ -1,28 +1,23 @@
-shots_count = int(input())
-shots = tuple(tuple(map(int, input().split())) for _ in range(shots_count))
+shots_count = 3
+shots = ((-1, -1, 1, 1), (-1, 0, 1, 1), (-1, -1, 0, 1))
 
-shots_data = {}
 sequence = [0] * shots_count
-
-for i in range(shots_count):
-    shots_data[i + 1] = tuple(shots[i])
 
 result_image = set()
 
-for i in range(shots_count, 0, -1):
+for i in range(-1, -shots_count - 1, -1):
 
-    x1, y1, x2, y2 = shots_data[i]
+    x1, y1, x2, y2 = shots[i]
     mesh = set()
-
+    
     for x in range(x1, x2):
         for y in range(y1, y2):
             mesh.add((x, y))
 
     result = len(mesh - result_image)
-    sequence[i * -1] = result
+    sequence[i] = result
     result_image.update(mesh)
 
 
-for i in sequence[::-1]:
-    print(i)
-
+for i in range(-shots_count, 0, 1):
+    print(sequence[i])
